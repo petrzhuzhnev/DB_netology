86b1af248ae38910cc02ab29573542bea6ab4167
Petr <petr.zhuzhnev@gmail.com>
2023-07-03 18:44:27 +0500

    first_fix

--- request_sql.sql
+++ request_sql.sql
@@ -9,17 +9,34 @@ create table album_executors(
 							id SERIAL primary key,
 							foreign key (id_album) references albums (id) on delete set null,
 							foreign key (id_executor) references executors (id) on delete set null);
+create table albums(
+					id SERIAL primary key,
+					title VARCHAR(50),
+					year_of_release DATE);
+create table executors(
+						id SERIAL primary key,
+						nickname VARCHAR(50));
+CREATE TABLE album_executors (
+    id SERIAL PRIMARY KEY,
+    id_album INTEGER,
+    id_executor INTEGER,
+    FOREIGN KEY (id_album) REFERENCES albums (id) ON DELETE SET NULL,
+    FOREIGN KEY (id_executor) REFERENCES executors (id) ON DELETE SET NULL
+);
 create table genre(
 					id SERIAL primary key,
 					name_of_genre VARCHAR(50));
 create table genre_executor(
 							id SERIAL primary key,
+							id_executor integer,
+							id_genre integer,
 							foreign key (id_executor) references executors (id) on delete set null,
 							foreign key (id_genre) references genre (id) on delete set null);
 create table track(
 					id SERIAL primary key,
 					title_track VARCHAR(50),
-					track_duration time,
+					track_duration integer,
+					id_album integer,
 					foreign key (id_album) references albums (id) on delete set null);
 create table collections(
 						id SERIAL primary key,
@@ -27,5 +44,7 @@ create table collections(
 						year_of_release DATE);
 create table cross_collections(
 								id SERIAL primary key,
+								id_track integer,
+								id_collecton integer,
 								foreign key (id_track) references track (id),
-								foreign key (id_collecton) references collections (id);
+								foreign key (id_collecton) references collections (id));
