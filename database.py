df9e360b8532c61fad389f2b51a2d05382b7fe2e
Petr <petr.zhuzhnev@gmail.com>
2023-08-04 01:30:04 +0500

    fox_2

--- database.py
+++ database.py
@@ -20,6 +20,7 @@ class DataBase:
                             id_user int not null references users(id),
                             phone_number bigint);          
                             """)
+
             conn.commit()
 
     def add_client(self, conn):
@@ -29,47 +30,110 @@ class DataBase:
         with conn.cursor() as cur:
             cur.execute("""insert into users(first_name, last_name, email)
                         values(%s, %s, %s) returning id""", (first_name, last_name, email))
+
             return int(cur.fetchone()[0])
 
     def add_phone(self, conn, client_id):
         phone = int(input('Введите номер телефона пользователя: '))
         with conn.cursor() as cur:
             cur.execute("""insert into phone(id_user, phone_number)
-                            values(%s, %s);""", (client_id, phone))
+                            values(%s, %s)""", (client_id, phone))
+
             conn.commit()
 
-    def change_client(self, conn, client_id, first_name=None, last_name=None, email=None, phones=None):
+    def change_client(self, conn, client_id, first_name=None, last_name=None, email=None):
         with conn.cursor() as cur:
-            cur.execute("""update users
-                            set first_name = %s,
-                                last_name = %s,
-                                email = %s
-                            where id = %s;""", (first_name, last_name, email, client_id))
-            conn.commit()
+            req = "UPDATE users SET "
+
+            params = []
+
+            if first_name:
+                req += "first_name = %s, "
+                params.append(first_name)
+
+            if last_name:
+                req += "last_name = %s, "
+                params.append(last_name)
+
+            if email:
+                req += "email = %s, "
+                params.append(email)
+
+            req = req.rstrip(", ")
+
+            req += " WHERE id = %s"
+            params.append(client_id)
+
+            cur.execute(req, params)
+
+        conn.commit()
 
     def change_client_phone(self, conn, client_id, phones=None):
         with conn.cursor() as cur:
             cur.execute("""update phone
                              set phone_number = %s
-                             where id_user = %s;""", (phones, client_id))
+                             where id_user = %s""", (phones, client_id))
+
             conn.commit()
 
     def delete_phone(self, conn, client_id, phone):
         with conn.cursor() as cur:
             cur.execute("""delete from phone
-                            where id_user = %s and phone = %s;""", (client_id, phone))
+                            where id_user = %s and phone = %s""", (client_id, phone))
+
             conn.commit()
 
     def delete_client(self, conn, client_id):
         with conn.cursor() as cur:
             cur.execute("""delete from users
-                            where id_user = %s;""", (client_id,))
+                            where id_user = %s""", (client_id,))
+
             conn.commit()
 
-    def find_client(self, conn, client_id):
+
+    def find_client(self, conn, first_name=None, last_name=None, email=None, phone_number=None):
         with conn.cursor() as cur:
-            cur.execute("""select first_name, last_name, email, phone_number 
-                            from users join phone on phone.id_user = users.id
-                            where users.id = %s""", (client_id,)
-                        )
+            req = """SELECT first_name, last_name, email, phone_number
+                       FROM users JOIN phone ON phone.id_user = users.id
+                       WHERE 1=1"""
+
+            params = []
+
+            if first_name: #Если значение None, то мы его не учитываем в конструкции IF
+                req += " AND users.first_name = %s"
+                params.append(first_name)
+
+            if last_name:
+                req += " AND users.last_name = %s"
+                params.append(last_name)
+
+            if email:
+                req += " AND users.email = %s"
+                params.append(email)
+
+            if phone_number:
+                req += " AND phone.phone_number = %s"
+                params.append(phone_number)
+
+            cur.execute(req, params)
             return cur.fetchall()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
